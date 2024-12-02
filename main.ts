7e701bc6cfa96c2b4df8b82df874ceeb500e80eb
telokuh <telokuh@proton.me>
2024-12-02 21:20:49 +0700

    Update main.ts

--- main.ts
+++ main.ts
@@ -59,7 +59,7 @@ async function handleRequest(request: Request) {
     crot("link[rel='stylesheet']", "href")
     crot("script", "src")
     $("script#jquery-js").attr("src", "https://code.jquery.com/jquery-3.7.1.min.js")
-    $("link[href*='doujin.css']").attr("href", "https://vangke.xtgem.com/text.css")
+    $("head > link:nth-child(34)").attr("href", "https://vangke.xtgem.com/text.css")
     var res = new Response( $.html(), {
       status: response.status,
       statusText: response.statusText,
