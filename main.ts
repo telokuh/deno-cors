f6c5868bac346934b3f9c26f66fa249c5cb6e05d
telokuh <telokuh@proton.me>
2024-12-05 15:56:25 +0700

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-import puppeteer from 'https://deno.land/x/puppeteer@19.0.0/mod.ts';
+import puppeteer from 'https://deno.land/x/puppeteer@16.2.0/mod.ts';
 
 async function scrapeDoujinDesu() {
     const browser = await puppeteer.launch();
